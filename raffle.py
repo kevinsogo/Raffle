ce59c704e340d5aca6f3ce80f448a03f68f92197
Kevin Atienza <kevin.charles.atienza@gmail.com>
2024-05-01 12:44:01 +0800

    add 'prizes'

--- raffle.py
+++ raffle.py
@@ -1,62 +1,107 @@
 from argparse import ArgumentParser
+from dataclasses import dataclass
 from itertools import islice
 import json
 from pathlib import Path
 from random import seed, shuffle
 from time import sleep
 
-parser = ArgumentParser()
 
-parser.add_argument('data_source', type=Path)
-parser.add_argument('-w', '--winners', type=int, default=3)
-parser.add_argument('-d', '--duplicates-allowed', action='store_true')
-parser.add_argument('-p', '--pause', type=float, default=1)
-parser.add_argument('-btw', '--best-to-worst', action='store_true')
+@dataclass
+class Data:
+    scores: dict[str, int]
+    prizes: list[str] | None = None
 
-args = parser.parse_args()
+    @property
+    def winner_count(self):
+        return len(self.prizes)
 
-with args.data_source.open() as f:
-    data = json.load(f)
 
-total_tickets = sum(data.values())
-print(f"The total number of tickets is {total_tickets}.")
-print(f"There will be {args.winners} winners.")
-seed(total_tickets)
+def load_data(path):
+    with path.open() as f:
+        data = json.load(f)
 
-if not args.duplicates_allowed and len(data) < args.winners:
-    raise RuntimeError("fewer candidates than winners")
+    # for backwards compatibility
+    if isinstance(data, dict) and all(isinstance(key, str) and isinstance(value, int) for key, value in data.items()):
+        data = {"scores": data}
 
-if total_tickets < args.winners:
-    raise RuntimeError("fewer tickets than winners")
+    return Data(**data)
 
-def candidates():
-    candidates = sorted(
-        name for name, score in data.items() for it in range(score)
-    )
-    shuffle(candidates)
-    return candidates
 
-def winners():
-    found = set()
-    for candidate in candidates():
-        if args.duplicates_allowed or candidate not in found:
-            found.add(candidate)
-            yield candidate
+def main():
+    parser = ArgumentParser()
 
-winners = [*islice(winners(), args.winners)]
+    parser.add_argument('data_source', type=Path)
+    parser.add_argument('-w', '--winners', type=int, default=None)
+    parser.add_argument('-d', '--duplicates-allowed', action='store_true')
+    parser.add_argument('-p', '--pause', type=float, default=1)
+    parser.add_argument('-btw', '--best-to-worst', action='store_true')
 
-if args.best_to_worst:
-    print('The ordering is from best to worst.\n')
-else:
-    print('The ordering is from worst to best.\n')
-    winners = [*reversed(winners)]
+    args = parser.parse_args()
 
-for candidate in winners:
-    print(end="And the winner is..", flush=True)
-    for it in range(3):
-        sleep(args.pause)
-        print(end='.', flush=True)
-    input()
-    print(f"{candidate}!", flush=True)
-    input()
+    data = load_data(args.data_source)
 
+    if args.winners is not None:
+        data.prizes = [None] * args.winners
+    elif data.prizes is None:
+        data.prizes = [None] * 3
+
+
+    total_tickets = sum(data.scores.values())
+    print(f"The total number of tickets is {total_tickets}.")
+    print(f"There will be {data.winner_count} winner(s).")
+    seed(total_tickets)
+
+    assert data.prizes is not None
+
+    if not args.duplicates_allowed and len(data.scores) < data.winner_count:
+        raise RuntimeError("fewer candidates than winners")
+
+    if total_tickets < data.winner_count:
+        raise RuntimeError("fewer tickets than winners")
+
+    def candidates():
+        candidates = sorted(
+            name for name, score in data.scores.items() for it in range(score)
+        )
+        shuffle(candidates)
+        return candidates
+
+    def winners():
+        found = set()
+        for candidate in candidates():
+            if args.duplicates_allowed or candidate not in found:
+                found.add(candidate)
+                yield candidate
+
+    winners = [*zip(winners(), reversed(data.prizes))]
+
+    if args.best_to_worst:
+        print('The ordering of winners is from best prize to worst prize.')
+    else:
+        print('The ordering of winners is from worst prize to best prize.')
+        winners = [*reversed(winners)]
+
+    print()
+
+    for candidate, prize in winners:
+        if prize is not None:
+            print(end="The next prize is... ", flush=True)
+            input()
+            print(f"{prize}")
+
+        print(end="And the winner is..", flush=True)
+
+        for it in range(3):
+            sleep(args.pause)
+            print(end='.', flush=True)
+        input()
+        print(f"{candidate}!", flush=True)
+        input()
+
+    print()
+    print("Congratulations!")
+    print()
+
+if __name__ == '__main__':
+    main()
